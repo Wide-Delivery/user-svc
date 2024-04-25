ad3d00780c2427194248125e79343f274c435153
Stanislav Matsak <72652632+matsakdev@users.noreply.github.com>
2024-04-26 01:21:47 +0300

    Minor improvements for better development process. (#1)

--- controllers/auth_controller.ts
+++ controllers/auth_controller.ts
@@ -1,6 +1,6 @@
 import {SignInUserInput__Output} from "../pb/auth/SignInUserInput";
 import {SignUpUserResponse} from "../pb/auth/SignUpUserResponse";
-import grpc, {sendUnaryData} from "@grpc/grpc-js";
+import * as grpc from "@grpc/grpc-js";
 import {createUser, isUserRegistered, refreshJwtTokens, signIn, signTokens} from "../services/user.service";
 import {PreCreatedUser, User, UserLoginDTO} from "../models/user";
 import {SignUpUserInput__Output} from "../pb/auth/SignUpUserInput";
@@ -10,14 +10,13 @@ import {RefreshTokenResponse} from "../pb/auth/RefreshTokenResponse";
 import {OAuthSignInInput__Output} from "../pb/auth/OAuthSignInInput";
 import {OAuth2Client} from "google-auth-library";
 import {OAUTH_PROVIDERS} from "../constants/oauth-providers";
-import UserModel from "../business-logic/schemas/user.schema";
 import {getUserByEmail} from "../repositories/User_repo";
 
 const client = new OAuth2Client();
 const GOOGLE_CLIENT_ID = '';
 const INTERNAL_ERROR_MESSAGE = 'Internal error. Contact with user-svc developer.';
 
-const processErrorInController = (err: any, callback: sendUnaryData<any>) => {
+const processErrorInController = (err: any, callback: grpc.sendUnaryData<any>) => {
     callback({
         code: err.code ? err.code : grpc.status.INTERNAL,
         message: err.message ? err.message : INTERNAL_ERROR_MESSAGE,
@@ -108,29 +107,32 @@ const verifyGoogleToken = async (token: string) => {
 
 async function verifyOAuthTokenAndGetUserInfo(oauthToken: string, provider: string) {
     let ticket;
-    switch (provider) {
-        case OAUTH_PROVIDERS.GOOGLE:
-            ticket = await verifyGoogleToken(oauthToken);
+    try {
+        switch (provider) {
+            case OAUTH_PROVIDERS.GOOGLE:
+                ticket = await verifyGoogleToken(oauthToken);
+        }
+
+        if (ticket) {
+            const payload = ticket.getPayload();
+            console.log('[user payload]', payload);
+            if (payload) {
+                const userId = payload['sub'];
+                return new PreCreatedUser({
+                    name: payload.name,
+                    email: payload.email,
+                    password: null,
+                    phoneNumber: '+380990000002',
+                    provider: OAUTH_PROVIDERS.GOOGLE,
+                    photo: payload.picture,
+                    role: 'user'
+                });
+            }
+        }
+    } catch (err) {
+        console.log(err)
     }
 
-    if (ticket) {
-        const payload = ticket.getPayload();
-        console.log('[user payload]', payload);
-        if (payload) {
-            const userId = payload['sub'];
-            return new PreCreatedUser({
-                name: payload.name,
-                email: payload.email,
-                password: null,
-                phoneNumber: '+380990000002',
-                provider: OAUTH_PROVIDERS.GOOGLE,
-                photo: payload.picture,
-                role: 'user'
-            });
-        }
-        // If request specified a G Suite domain:
-        // const domain = payload['hd'];
-    }
 }
 
 export const oAuthSignInHandler = async (
