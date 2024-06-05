150486e1dd32529356a627abc5a21107a0f87f9e
smatsak <stanislav.matsak@nure.ua>
2024-06-05 17:27:59 +0300

    Update pb-folder

---
+++ pb/auth/GetUserByIdInput.ts
@@ -0,0 +1,10 @@
+// Original file: protos/services.proto
+
+
+export interface GetUserByIdInput {
+  'user_id'?: (string);
+}
+
+export interface GetUserByIdInput__Output {
+  'user_id': (string);
+}
---
+++ pb/auth/GetUsersWithFiltersRequest.ts
@@ -0,0 +1,34 @@
+// Original file: protos/services.proto
+
+
+export interface GetUsersWithFiltersRequest {
+  'id'?: (string);
+  'name'?: (string);
+  'email'?: (string);
+  'phone_number'?: (string);
+  'provider'?: (string);
+  'role'?: (string);
+  'photo'?: (string);
+  'created_at_before'?: (string);
+  'created_at_after'?: (string);
+  'sort'?: (string);
+  'sortDirection'?: (string);
+  'limit'?: (number);
+  'offset'?: (number);
+}
+
+export interface GetUsersWithFiltersRequest__Output {
+  'id': (string);
+  'name': (string);
+  'email': (string);
+  'phone_number': (string);
+  'provider': (string);
+  'role': (string);
+  'photo': (string);
+  'created_at_before': (string);
+  'created_at_after': (string);
+  'sort': (string);
+  'sortDirection': (string);
+  'limit': (number);
+  'offset': (number);
+}
